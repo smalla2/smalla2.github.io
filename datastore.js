197c37b1dd94adefb9d7135e0a3009a6107d8ea2
smalla2 <32131576+smalla2@users.noreply.github.com>
2019-07-31 16:12:13 -0500

    Create datastore.js

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+    {
+      question: 'What position did Zinedine Zidane play?',
+      answers: [
+        'striker',
+        'left winger',
+        'playmaker',
+        'leftback'
+      ],
+      correctAnswer: 'playmaker',
+    },
+    {
+      question: 'How many goals did Zidane score in the 1998 World Cup Final?',
+      answers: [
+        '0',
+        '1',
+        '2',
+        '3'
+      ],
+      correctAnswer: '2',
+    },
+    {
+      question: "What is Zinedine Zidane's nickname?",
+      answers: [
+        'El Fenomenon',
+        'Magic Chris',
+        'The King',
+        'Zizou'
+      ],
+      correctAnswer: 'Zizou',
+    },
+    {
+      question: 'Which team did Zidane never play for?',
+      answers: [
+        'Algeria',
+        'France',
+        'Juventus',
+        'Bordeaux'
+      ],
+      correctAnswer: 'Algeria',
+    }, 
+    {
+      question: 'How many Champions League trophies did he win as a player',
+      answers: [
+        '0',
+        '1',
+        '2',
+        '3'
+      ],
+      correctAnswer: '1',
+    },
+    {
+      question: 'How many Champions League trophies did he win as a manager',
+      answers: [
+        '0',
+        '1',
+        '2',
+        '3'
+      ],
+      correctAnswer: '3',
+    },
+    {
+      question: "How many Ballon d'Or and FIFA player of the year awards does Zidane have?",
+      answers: [
+        '1',
+        '2',
+        '3',
+        '4'
+      ],
+      correctAnswer: '4',
+    },
+    {
+      question: 'Which was his last competitive match?',
+      answers: [
+        '2001 Champions league final',
+        '2006 final La Liga game',
+        'Euro 2004 semin-final',
+        '2006 World Cup final'
+      ],
+      correctAnswer: '2006 World Cup final',
+    },
+    {
+      question: "What is Zidane's signature move called?",
+      answers: [
+        'Panenka',
+        'Pedalada step over',
+        'La Roulette/the Marseille turn',
+        'Elastico'
+      ],
+      correctAnswer: 'La Roulette/the Marseille turn',
+    },
+    {
+      question: 'Who is my favorite player of all time?',
+      answers: [
+        'Lionel Messi',
+        'Christian Ronaldo',
+        'Luka Modric',
+        'Zizou'
+      ],
+      correctAnswer: 'Zizou',
+    }
+];
